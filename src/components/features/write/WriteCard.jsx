a8ff8bc8e1ffd2a3458c88541144b7bd187a36ba
Kim <kimmy199535@gmail.com>
2022-12-13 10:42:47 +0900

    1️⃣  first commit

--- src/components/features/write/WriteCard.jsx
+++ src/components/features/write/WriteCard.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
+import { CKEditor } from 'ckeditor4-react';
 
 // 일기쓰기/수정 페이지에 들어갈 컴포넌트
 // LinkDiv 테스트용
@@ -12,7 +13,16 @@ function CardList() {
         <Link to="/" className="datail">
           <span>일기 그만쓰기</span>
         </Link>
+        <button type="submit">일기 저장</button>
       </LinkDiv>
+      <StInputContainer>
+        <StInput type="text" placeholder="제목을 작성해주세요." />
+        <CKEditor
+          onInstanceReady={() => {
+            console.log('Editor is ready!');
+          }}
+        />
+      </StInputContainer>
     </DiaryWrite>
   );
 }
@@ -26,11 +36,25 @@ const DiaryWrite = styled.div`
 `;
 
 const LinkDiv = styled.div`
-  display: block;
-  margin: 10px 0 0 20px;
+  display: flex;
+  margin: auto;
   font-size: 16px;
   a {
     color: #949393;
     text-decoration: none;
   }
+  justify-content: space-between;
+`;
+
+const StInputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 50px;
+
+  padding: 100px 100px 100px 100px;
+`;
+
+const StInput = styled.input`
+  border: none;
+  padding: 20px;
 `;
